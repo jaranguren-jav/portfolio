069655d6b74e74172171fb02d89c89c0b8214385
Julen <jaranguren.jav@gmail.com>
2020-04-16 20:12:41 +0200

    Updates

---
+++ precache-manifest.16e7ccde86d29482aa3380c3e0108157.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c4357fa02ac3dabfce57cbed70536d4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c5596df98c5b91e95f16",
+    "url": "/portfolio/static/css/main.2e4531a1.chunk.css"
+  },
+  {
+    "revision": "7311f81d109e0b6e6abb",
+    "url": "/portfolio/static/js/2.0c9d3d7c.chunk.js"
+  },
+  {
+    "revision": "f89048c398bfcfcc6dc1f4ae8f0e5c36",
+    "url": "/portfolio/static/js/2.0c9d3d7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5596df98c5b91e95f16",
+    "url": "/portfolio/static/js/main.7f528b62.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  }
+]);
